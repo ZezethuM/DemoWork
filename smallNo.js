693f9b2ed8ab51ffbe1f75a9a2be36635e34260e
Zezethu <zezethu15@gmail.com>
2022-03-08 10:37:39 +0200

    Code to find small No in Array

---
+++ smallNo.js
@@ -0,0 +1,9 @@
+function arrayPlusArray(arr1, arr2) {
+    
+  var arr3 = arr1.concat(arr2);
+  var sum = 0;
+  for(var i = 0; i < arr3.length; i++){
+    sum = sum + arr3[i];
+  }
+  return sum;
+}
